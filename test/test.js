1e900850e03c97ea2f08ba594c4b1a95d3723d54
yashw1202 <yashwathreys@gmail.com>
2024-03-26 23:04:26 +0530

    Web dev practice codes uploading

---
+++ test/test.js
@@ -0,0 +1,7 @@
+let age ;
+age = 21 + 1;
+var firstname = "Yash";
+console.log(age);
+console.log(firstname);
+document.getElementById("p1").innerHTML = "HELLO I am " + age; 
+document.getElementById("p2").innerHTML = "My Name is " + firstname ;
